1240ca456a2bde9cf661cf8f1912eda39bbeb382
chanthea <taichanthea@gmail.com>
2020-03-06 17:08:20 +0700

    fix listener

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+
 // packages
-import { Dimensions, PixelRatio } from 'react-native';
+import { Dimensions, PixelRatio } from "react-native";
 
 // Retrieve initial screen's width
-let screenWidth = Dimensions.get('window').width;
+let screenWidth = Dimensions.get("window").width;
 
 // Retrieve initial screen's height
-let screenHeight = Dimensions.get('window').height;
+let screenHeight = Dimensions.get("window").height;
 
 /**
  * Converts provided width percentage to independent pixel (dp).
@@ -15,11 +16,12 @@ let screenHeight = Dimensions.get('window').height;
  */
 const widthPercentageToDP = widthPercent => {
   // Parse string percentage input and convert it to number.
-  const elemWidth = typeof widthPercent === "number" ? widthPercent : parseFloat(widthPercent);
+  const elemWidth =
+    typeof widthPercent === "number" ? widthPercent : parseFloat(widthPercent);
 
   // Use PixelRatio.roundToNearestPixel method in order to round the layout
   // size (dp) to the nearest one that correspons to an integer number of pixels.
-  return PixelRatio.roundToNearestPixel(screenWidth * elemWidth / 100);
+  return PixelRatio.roundToNearestPixel((screenWidth * elemWidth) / 100);
 };
 
 /**
@@ -30,15 +32,18 @@ const widthPercentageToDP = widthPercent => {
  */
 const heightPercentageToDP = heightPercent => {
   // Parse string percentage input and convert it to number.
-  const elemHeight = typeof heightPercent === "number" ? heightPercent : parseFloat(heightPercent);
+  const elemHeight =
+    typeof heightPercent === "number"
+      ? heightPercent
+      : parseFloat(heightPercent);
 
   // Use PixelRatio.roundToNearestPixel method in order to round the layout
   // size (dp) to the nearest one that correspons to an integer number of pixels.
-  return PixelRatio.roundToNearestPixel(screenHeight * elemHeight / 100);
+  return PixelRatio.roundToNearestPixel((screenHeight * elemHeight) / 100);
 };
 
 /**
- * Event listener function that detects orientation change (every time it occurs) and triggers 
+ * Event listener function that detects orientation change (every time it occurs) and triggers
  * screen rerendering. It does that, by changing the state of the screen where the function is
  * called. State changing occurs for a new state variable with the name 'orientation' that will
  * always hold the current value of the orientation after the 1st orientation change.
@@ -46,17 +51,21 @@ const heightPercentageToDP = heightPercent => {
  * @param {object} that Screen's class component this variable. The function needs it to
  *                      invoke setState method and trigger screen rerender (this.setState()).
  */
+
 const listenOrientationChange = that => {
-  Dimensions.addEventListener('change', newDimensions => {
+  //Save the handler to a property in the Component
+  that.orientationChangeHandler = newDimensions => {
     // Retrieve and save new dimensions
     screenWidth = newDimensions.window.width;
     screenHeight = newDimensions.window.height;
 
     // Trigger screen's rerender with a state update of the orientation variable
     that.setState({
-      orientation: screenWidth < screenHeight ? 'portrait' : 'landscape'
+      orientation: screenWidth < screenHeight ? "portrait" : "landscape"
     });
-  });
+  };
+
+  Dimensions.addEventListener("change", that.orientationChangeHandler);
 };
 
 /**
@@ -65,8 +74,8 @@ const listenOrientationChange = that => {
  * listenOrientationChange function has been invoked. This should be done in order to
  * avoid adding new listeners every time the same component is re-mounted.
  */
-const removeOrientationListener = () => {
-  Dimensions.removeEventListener('change', () => {});
+const removeOrientationListener = that => {
+  Dimensions.removeEventListener("change", that.orientationChangeHandler);
 };
 
 export {
